6e3abe1ee3ef77461986c387db1bc895443fd304
Qutbuddin Mazahir Husain Millwala <159757453+qutubh53@users.noreply.github.com>
2024-02-13 09:33:56 +0000

    worked on Stack and Heap memory, String Prototypes and Math Values

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,41 @@
-console.log("Qutbuddin");
+// console.log("Qutbuddin");
+
+// Stack Memory (Primitive)
+/* let firstName = "Rahul"
+let secondName = firstName
+secondName = "Kishan"
+
+console.log(firstName);
+console.log(secondName); */
+
+
+// Heap Memory (Non-Primitive)
+/* let thirdName = {
+    userName: "Harsh",
+    age: 21
+}
+
+let fourthName = thirdName
+fourthName.userName = "Manish"
+fourthName.age = 25
+
+console.log(`Hello ${thirdName.userName} Your Age is ${thirdName.age}`);
+console.log(`Hello ${fourthName.userName} Your Age is ${fourthName.age}`); */
+
+
+// String Prototypes
+/* let stringOne = "Amit"
+console.log(stringOne);
+
+let stringTwo = new String("Mohan")
+console.log(stringTwo.slice(0,3));
+console.log(stringTwo.replace("Mohan", "Virat"));
+console.log(stringTwo.sub());
+console.log(stringTwo.toUpperCase());
+console.log(stringTwo.trim()); */
+
+let minValue = 0
+let MaxValue = 6
+let diesNumber = Math.floor(Math.random()* (MaxValue - minValue) + 1)
+
+// console.log(diesNumber);
